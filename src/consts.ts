0e017dec78c4160fcf3dd813b297c3d74966c19b
Sandor Trombitas <sandor@Sandors-Laptop.local>
2023-10-16 23:23:00 +0300

    Init: Initialise project with astro blog template

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Sandors Website';
+export const SITE_DESCRIPTION = 'A place for my thoughts!';
