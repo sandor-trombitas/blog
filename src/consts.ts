6fa7276b30d6f30d93aaffa667a1404a08ab49b0
Sandor Trombitas <tis.sandor@gmail.com>
2023-11-01 19:56:56 +0200

    Change(links): Update social media links and name

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Sandors Website';
-export const SITE_DESCRIPTION = 'A place for my thoughts!';
+export const SITE_TITLE = '1d6.dev';
+export const SITE_DESCRIPTION = '1d6.dev';
